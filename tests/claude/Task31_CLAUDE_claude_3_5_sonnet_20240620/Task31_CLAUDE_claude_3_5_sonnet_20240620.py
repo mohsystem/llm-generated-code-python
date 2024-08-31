e10d107554a75e0f68d04c45dcdf0ab38150fe6d
mohkharma <moh.kharma@gmail.com>
2024-08-31 04:01:52 +0300

    Fix TCs for problem 31

--- tests/claude/Task31_CLAUDE_claude_3_5_sonnet_20240620/Task31_CLAUDE_claude_3_5_sonnet_20240620.py
+++ tests/claude/Task31_CLAUDE_claude_3_5_sonnet_20240620/Task31_CLAUDE_claude_3_5_sonnet_20240620.py
@@ -1,11 +1,11 @@
-# from output.claude.Task31_CLAUDE_claude_3_5_sonnet_20240620 import DNA_strand as make_complement
+from output.claude.Task31_CLAUDE_claude_3_5_sonnet_20240620 import DNA_strand as make_complement
 # from output.codestral.Task31_MISTRAL_codestral_latest import dna_complement as make_complement
-from output.gemini.Task31_GEMINI_gemini_1_5_pro_001 import DNA_strand as make_complement
+# from output.gemini.Task31_GEMINI_gemini_1_5_pro_001 import DNA_strand as make_complement
 # from output.gtp4o.Task31_OPENAI_gpt_4o import dna_strand as make_complement
 # from output.llama3.Task31_PERPLEXITY_llama_3_sonar_large_32k_chat import DNA_strand as make_complement
 import unittest
 
-# todo : need edit !
+
 class TestMakeComplement(unittest.TestCase):
 
     def test_complementary_strand_1(self):
@@ -46,7 +46,7 @@ class TestMakeComplement(unittest.TestCase):
 
     def test_mixed_case(self):
         # Test case with a mixed-case DNA strand
-        self.assertEqual(make_complement("attgc".lower()), "TAACG")
+        self.assertEqual(make_complement("attgc".upper()), "TAACG")
 
 if __name__ == '__main__':
     unittest.main()
